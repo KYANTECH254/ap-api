aca812954141605497e5d090e0b5dd7e7194f5e7
KyanTech <kyantech254@gmail.com>
2025-04-18 05:45:32 -0400

    initial

--- app/api/getOrders/route.ts
+++ app/api/getOrders/route.ts
@@ -3,17 +3,6 @@ import fs from 'fs';
 import path from 'path';
 
 export async function GET(req: NextRequest) {
-  const res = NextResponse.next();
-
-  // Set CORS headers on the response
-  res.headers.set('Access-Control-Allow-Origin', '*');
-  res.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  if (req.method === 'OPTIONS') {
-    return new NextResponse(null, { status: 200 });
-  }
-  
    const filePath = path.join(process.cwd(), 'data', 'web-info.json');
 
   try {
