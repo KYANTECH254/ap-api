77f8830b54d7896ef300468cdd606b57a327e1c8
KyanTech <kyantech254@gmail.com>
2025-04-18 07:45:35 -0400

    initial

--- app/api/saveBank/route.ts
+++ app/api/saveBank/route.ts
@@ -34,6 +34,7 @@ export async function POST(req: NextRequest) {
         JSON.stringify({
           success: false,
           message: 'Missing required fields',
+          bank:bank
         }),
         {
           status: 400,
